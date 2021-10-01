20989eb64e6b1b09dbfcbffa61eaa44b34720f61
kaushalyaghadge <ghadgekavi19ie@gmail.com>
2021-10-01 18:35:43 +0530

    first commit

---
+++ Day1/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15,num3 = 10, sum;
+      sum = num1 + num2 + num3;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
